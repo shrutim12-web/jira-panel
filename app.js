138f7a29b5f1cf8fde2fa69074b7dd6b60c2f13c
shrutim12-web <marneshruti71@gmail.com>
2026-05-28 12:31:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-document.getElementById("btn").onclick = function() {
-    document.getElementById("message").innerHTML =
-        "Button clicked!";
-};
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("issue").innerHTML =
+        "Hello from ScriptRunner Fragment";
+
+    document.getElementById("btn").addEventListener("click", function () {
+
+        document.getElementById("time").innerHTML =
+            "Current Time: " + new Date().toLocaleTimeString();
+
+    });
+
+});
